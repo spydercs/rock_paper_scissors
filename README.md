b64242486fb6622022fa1153259dcc26f1925a27
spydercs <spydercs@gmail.com>
2024-07-10 09:15:20 -0700

    added a function playGame to play 5 rounds and then report the final score back.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rock_paper_scissors
 a console based rock-paper-scissors game
+
+This is a rock paper scissors game that is played entirely with pop-up messages. It will play 5 rounds and then tell you the final score.
+
--- javascript.js
+++ javascript.js
@@ -3,19 +3,17 @@
  */
 
 function getComputerChoice() {
-  let choice = Math.random()
-  console.log("computer choice number is " + choice)
-  if (choice > 0.66667) {
+  let computerChoice = Math.ceil(Math.random() * 3)
+  if (computerChoice === 1) {
     return("rock")
   } else {
-    if (choice < 0.33333) {
+    if (computerChoice === 2) {
       return("paper")
     } else {
       return("scissors")
     }
   }
 }
-console.log("Computer choice is " + getComputerChoice())
 
 function getHumanChoice() {
   let humanChoice = prompt("Please type in (r)ock, (p)aper or (s)cissors")
@@ -40,4 +38,57 @@ function getHumanChoice() {
   }
 }
 
-console.log("Human choice is " + getHumanChoice())
+let humanScore = 0
+
+let computerScore = 0 
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock") {
+    if (computerChoice === "rock") {
+      return("It's a tie! You both chose rock.")
+    } else if (computerChoice === "paper"){
+      ++computerScore
+      return("You lose! Paper beats rock.")
+    } else {
+      //Computer chooses scissors
+      ++humanScore
+      return("You win! Rock beats scissors.")
+    }
+  } else if (humanChoice ==="paper") {
+    if (computerChoice === "rock") {
+      ++humanScore
+      return("You win! Paper beats rock.")
+    } else if (computerChoice === "paper"){
+      return("It's a tie! You both chose paper.")
+    } else {
+      //Computer chooses scissors
+      ++computerScore
+      return("You Lose! Scissors beats paper.")
+    }
+  } else {
+    //human chooses scissors
+    if (computerChoice === "rock") {
+      ++computerScore
+      return("You lose! Rock beats scissors.")
+    } else if (computerChoice === "paper"){
+      ++humanScore
+      return("You win! Scissors beats paper.")
+    } else {
+      //Computer chooses scissors
+      return("It's a tie! You both chose scissors.")
+    }
+
+  }
+
+}
+
+function playGame() {
+  alert(playRound(getHumanChoice(),getComputerChoice()))
+  alert(playRound(getHumanChoice(),getComputerChoice()))
+  alert(playRound(getHumanChoice(),getComputerChoice()))
+  alert(playRound(getHumanChoice(),getComputerChoice()))
+  alert(playRound(getHumanChoice(),getComputerChoice()))
+  alert("human score " + humanScore + ", computer score " + computerScore)
+}
+
+playGame()
